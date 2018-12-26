73e7cba246941e61bbcbdde1a8fac3aaba47fa30
Samantha Pelé <williamdutupem@gmail.com>
2018-12-26 16:48:34 +0100

    getX

--- server/delire/BDD.py
+++ server/delire/BDD.py
@@ -1,10 +1,14 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, logout, authenticate
 
 from .models import Noeud, Personne, Employe, Patient, Specialite, Document, Ecrit, EmpSpe, NoeudSpe
 
 import base64
 
 class BDD(object):
+	TYPE_DOCUMENT = 1
+	TYPE_PERSONNE = 2
+	TYPE_NOEUD    = 3
+	
 	def _encrypt(self, data):
 		"""Encode le paramètre data(str ou bytes) en base64 et retourne
 		   l'objet bytes renvoyé."""
@@ -26,6 +30,51 @@ class BDD(object):
 		
 		return base64.b64decode(dataEnc)
 		
-	def login(self, username, password):
-		"""Verify if the login and the password are good and return the user id if good."""
+	def login(self, username, password, request):
+		"""Vérifie si le username et le password sont bons et si oui le connecte dans Django."""
+		user = authenticate(request, username, password)
 		
+		if user is not None:
+			login(request, user)
+			return True
+		
+		return False
+		
+	def getJob(self, idPersonne):
+		"""Renvoie le Job"""
+		pass
+		
+	def _getX(self, idX, typeX):
+		"""Méthode interne gérant les 3 types de get voulus."""
+		if typeX not in (self.TYPE_DOCUMENT, self.TYPE_PERSONNE, self.TYPE_NOEUD):
+			raise ValueError("Le paramètre typeX doit être égal à une des 3 contantes de type")
+		
+		if typeX == self.TYPE_DOCUMENT:
+			obj = Document
+		elif typeX == self.TYPE_PERSONNE:
+			obj = Personne
+		else:
+			obj = Noeud
+			
+		try:
+			x = obj.objects.get(idX)
+		except:
+			x = None
+		
+		return x
+		
+	def _createX(self, typeX, **kwargs):
+		pass
+	
+	def getDocument(self, id):
+		"""Renvoie le document correspondant à l'id donné en paramètre ou None s'il n'est pas dans la BDD"""
+		return self._getX(id, self.TYPE_DOCUMENT)
+		
+	def getPersonne(self, id):
+		"""Renvoie la personne correspondant à l'id donné en paramètre ou None si elle n'est pas dans la BDD"""
+		return self._getX(id, self.TYPE_PERSONNE)
+		
+	def getNoeud(self, id):
+		"""Renvoie le noeud correspondant à l'id donné en paramètre ou None s'il n'est pas dans la BDD"""
+		return self._getX(id, self.TYPE_NOEUD)
+	

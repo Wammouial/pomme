b09ed49dc829a545acd319db49081944f2343b4e
William <williamdutupem@gmail.com>
2019-02-11 21:21:16 +0100

    insertBDD

--- server/delire/dataBDD.py
+++ server/delire/dataBDD.py
@@ -1,5 +1,7 @@
 import sys
 import pandas
+import math
+import numpy as np
 
 EXCEL_FILE = "data.xlsx" if len(sys.argv) < 2 else sys.argv[1] 
 
@@ -8,8 +10,22 @@ SHEETS = [ "PersonneClient", "PersonneAPHP", "Noeud", "Spécialité",
 
 def getDF(file, sheet):
 	return pandas.read_excel(file, sheet_name=sheet)
+	
+def treatRow(row, labels, table):
+	kwargs = {}
+	for y in labels:
+		data = row.loc[0, y]
+		
+		if type(data) == float or type(data) == np.float64:
+
+			if math.isnan(data):
+				continue
+		kwargs[y] = data
+	
+	return kwargs
 
 if __name__ == "__main__":
 	dfs = {x : getDF(EXCEL_FILE, x) for x in SHEETS}
+	
 	
 	

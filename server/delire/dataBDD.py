1193e46e6400cfab350b8fccd896e161c79dc41b
Wammouial <williamdutupem@gmail.com>
2019-02-11 19:35:43 +0100

    dataBDD

--- server/delire/dataBDD.py
+++ server/delire/dataBDD.py
@@ -1,2 +1,15 @@
+import sys
 import pandas
 
+EXCEL_FILE = "data.xlsx" if len(sys.argv) < 2 else sys.argv[1] 
+
+SHEETS = [ "PersonneClient", "PersonneAPHP", "Noeud", "Spécialité",
+		   "Document", "Ecrit", "EmpSpe", "NoeudSpe" ]
+
+def getDF(file, sheet):
+	return pandas.read_excel(file, sheet_name=sheet)
+
+if __name__ == "__main__":
+	dfs = {x : getDF(EXCEL_FILE, x) for x in SHEETS}
+	
+	

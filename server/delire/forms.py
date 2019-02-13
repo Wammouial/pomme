b349bcecd4feda0014260ff660b9f3c87f450324
Mathilde Corradi <mathilde@macbook-pro-de-mathilde.home>
2019-02-13 10:11:31 +0100

    ajout fichier forms.py

---
+++ server/delire/forms.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from django import forms
+
+class ContactForm(forms.Form):
+    sujet = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
+    envoyeur = forms.EmailField(label="Votre adresse e-mail")
+    renvoi = forms.BooleanField(help_text="Cochez si vous souhaitez obtenir une copie du mail envoyé.", required=False)
+

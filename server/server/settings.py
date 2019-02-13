a6fa595b9b5a3b37597dc648638f22d0a7fb492b
Mathilde Corradi <mathilde@macbook-pro-de-mathilde.home>
2019-02-13 22:56:19 +0100

    correction settings bis

--- server/server/settings.py
+++ server/server/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'delire', 'templates'),
+        'DIRS': [os.path.join(BASE_DIR, 'delire', 'templates/delire'),
 		],
         'APP_DIRS': True,
         'OPTIONS': {

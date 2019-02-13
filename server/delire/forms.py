48b8cb563ff3d6c51a211b1bbbef3a691f251508
Mathilde Corradi <mathilde@MacBook-Pro-de-Mathilde.local>
2019-02-13 16:21:59 +0100

    update formulaire patient

--- server/delire/forms.py
+++ server/delire/forms.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from django import forms
+import datetime
 
-class ContactForm(forms.Form):
-    sujet = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea)
-    envoyeur = forms.EmailField(label="Votre adresse e-mail")
-    renvoi = forms.BooleanField(help_text="Cochez si vous souhaitez obtenir une copie du mail envoyé.", required=False)
+class PatientForm(forms.Form):  #formulaire patient modèle ?
+    CHOICES=[('select1','F'),
+         ('select2','M')]
+    nom = forms.CharField(max_length=100)
+    prenom = forms.CharField(max_length=100)
+    sexe = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+    date_de_naissance = forms.DateField(initial=datetime.date.today)
+    numéro_de_securité_sociale = forms.CharField(max_length=100)
+    numéro = forms.CharField(max_length=3)
+    libellé = forms.CharField(max_length=70)
+    code_postal = forms.CharField(max_length=5)
+    ville = forms.CharField(max_length=100)
+    #message = forms.CharField(widget=forms.Textarea)
+    adresse_email = forms.EmailField(label="Adresse e-mail")
+    nationalité = forms.CharField(max_length=30)
+    numéro_de_téléphone = forms.CharField(max_length=10)
+    situation_familiale = forms.CharField(max_length=70)
+    #renvoi = forms.BooleanField(help_text="Cochez si vous souhaitez obtenir une copie du mail envoyé.", required=False)
 
--- server/delire/views.py
+++ server/delire/views.py
@@ -2,33 +2,39 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from .BDD import BDD
-from .forms import ContactForm
+from .forms import PatientForm
 
 # Create your views here.
 
-def form(request):
+def form(request): #formulaire patient
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = PatientForm(request.POST)
         # Nous vérifions que les données envoyées sont valides
         # Cette méthode renvoie False s'il n'y a pas de données
         # dans le formulaire ou qu'il contient des erreurs.
         if form.is_valid():
             # Ici nous pouvons traiter les données du formulaire
-            sujet = form.cleaned_data['sujet']
-            message = form.cleaned_data['message']
-            envoyeur = form.cleaned_data['envoyeur']
-            renvoi = form.cleaned_data['renvoi']
-            
-            # Nous pourrions ici envoyer l'e-mail grâce aux données
-            # que nous venons de récupérer
-            envoi = True
+            nom = form.cleaned_data['nom']
+            prenom = form.cleaned_data['prenom']
+            sexe = form.cleaned_data['sexe']
+            date_de_naissance = form.cleaned_data['date_de_naissance']
+            numéro_de_securité_sociale = form.cleaned_data['numéro_de_securité_sociale']
+            numéro = form.cleaned_data['numéro']
+            libellé = form.cleaned_data['libellé']
+            code_postal = form.cleaned_data['code_postal']
+            ville = form.cleaned_data['ville']
+            adresse_email = form.cleaned_data['adresse_email']
+            nationalité = form.cleaned_data['nationalité']
+            numéro_de_téléphone = form.cleaned_data['numéro_de_téléphone']
+            situation_familiale = form.cleaned_data['situation_familiale']
+            #message = form.cleaned_data['message']
         
         # Quoiqu'il arrive, on affiche la page du formulaire.
         #return render(request, 'PommeApp/templates/template1.py', locals())
         return HttpResponseRedirect('/thanks/')
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = ContactForm()
+        form = PatientForm()
     
     return render(request, 'patient.html', {'form': form})
 

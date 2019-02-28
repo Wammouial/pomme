d12c41c066a01ebabd8c44746f071c5dfc1f5924
Mathilde Corradi <mathilde@MacBook-Pro-de-Mathilde.local>
2019-02-28 15:35:35 +0100

    views

--- server/delire/views.py
+++ server/delire/views.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 28 14:40:00 2019
-
-@author: mathilde
-"""
-
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 

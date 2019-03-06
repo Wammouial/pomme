653436203fd08efa0273f114e6ce7f736f27f19e
RemiApruzzese <44636266+RemiApruzzese@users.noreply.github.com>
2019-03-06 17:50:14 +0100

    Add files via upload

--- server/delire/forms.py
+++ server/delire/forms.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from django.core.exceptions import NON_FIELD_ERRORS
-from django.forms import ModelForm, Textarea, TextInput
+from django.forms import ModelForm, Textarea, TextInput, IntegerField
 from django import forms
 from .models import Personne
 
@@ -49,17 +49,59 @@ class PatientFormCreate(ModelForm):
 				  'situationFamiliale',
 				  'email',
 				  ]
+
+class PersonnelFormEdit(ModelForm):
+	def __init__(self, *args, **kwargs):
+		super(PersonnelFormEdit, self).__init__(*args, **kwargs)
+		for f in ('job', 'situationFamiliale', 'telephone', 'lieuNaissance'):
+			self.fields[f].required = False
+			
+		for f in self.fields:
+			self.fields[f].widget.attrs.update({'class': 'form-control'})
+	
+	class Meta:
+		model = Personne
+		fields = ['nom',
+				  'prenom',
+				  'sexe',
+				  'dateNaissance',
+				  'lieuNaissance',
+				  'numSS',
+				  'adresse',
+				  'telephone',
+				  'situationFamiliale',
+				  ]
+        
+class PersonnelFormCreate(ModelForm):
+	def __init__(self, *args, **kwargs):
+		super(PersonnelFormCreate, self).__init__(*args, **kwargs)
+		for f in ('situationFamiliale', 'telephone', 'lieuNaissance'):
+			self.fields[f].required = False
+			
+		for f in self.fields:
+			self.fields[f].widget.attrs.update({'class': 'form-control'})
+	
+	class Meta:
+		model = Personne
+		fields = ['nom',
+				  'prenom',
+				  'sexe',
+				  'dateNaissance',
+				  'lieuNaissance',
+				  'job',
+				  'adresse',
+				  'telephone',
+				  'situationFamiliale',
+				  'email',
+				  ]
 		
-class RechercheForm(forms.Form):
+class RechercheFormPatient(forms.Form):
 	nom = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
 	prenom = forms.CharField(max_length=255, required=False, widget=forms.TextInput(attrs={'class' : 'form-control'}))
 	numSS = forms.CharField(max_length=15, required=False, widget=forms.TextInput(attrs={'class' : 'form-control'}))
 	
-class RechercheNoeudForm(forms.Form):
-    idNoeud = forms.CharField(max_length=255)
-        
-class NoeudFormEdit(forms.Form):
-    nom = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    typ = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    boss_id = forms.CharField(max_length=15, required=False, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    pere_id = forms.CharField(max_length=15, required=False, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+class RechercheFormPersonnel(forms.Form):
+	nom = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+	prenom = forms.CharField(max_length=255, required=False, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+	job = forms.IntegerField(required=False, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+	
